98e401e47a1fde855025835d17476faf3300b6a9
kapil619 <kapilbadokar321@gmail.com>
2023-08-20 19:25:49 +0530

    Added a Modal Dropdown

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -26,12 +26,21 @@ class _ExpensesState extends State<Expenses> {
       category: Category.bills,
     ),
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => const Text('Modal here'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense Tracker'),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+        actions: [
+          IconButton(
+              onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
+        ],
       ),
       body: Column(
         children: [
