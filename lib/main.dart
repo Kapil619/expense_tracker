e958a61e42b25c785ef6a74241c529122bae77bc
kapil619 <kapilbadokar321@gmail.com>
2023-08-24 08:07:09 +0530

    Locking the device in Portrait mode [commented]

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
@@ -11,6 +12,9 @@ var kDarkColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 214, 0, 237),
 );
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+  //     .then((fn) {
   runApp(
     MaterialApp(
         darkTheme: ThemeData.dark().copyWith(
@@ -56,4 +60,6 @@ void main() {
         // themeMode: ThemeMode.system, default
         home: const Expenses()),
   );
+  // }
+  // );
 }
